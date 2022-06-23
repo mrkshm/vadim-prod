a832b1e87bd4e15a86e06a1b1a52bfed5530b6f1
mrks <hey@mrks.me>
2022-06-23 10:49:42 +0200

    no more store

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,12 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
-import { StoreProvider } from "easy-peasy";
 import Layout from "../components/Layout";
-import { store } from "../src/utils/store";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <StoreProvider store={store}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </StoreProvider>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
 
